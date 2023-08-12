160a83350723545051ed82a2c5c8d408abcf6434
Takayuki Kamiyama <karuma.reason@gmail.com>
2023-08-12 14:18:23 +0900

    Update.

---
+++ neo_win_install.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'open3'
+require 'fileutils'
+
+class White
+  # default encoding utf-8, change encode here.
+  def self.encoding_style
+    Encoding.default_internal = 'UTF-8'
+    Encoding.default_external = 'UTF-8'
+  end
+
+  def self.magic
+  encoding_style
+  if File.exist?(File.expand_path('~/AppData/Local/nvim/init.vim'))
+    FileUtils.rm_rf(File.expand_path('~/AppData/Local/nvim/init.vim'))
+  end
+    myhome = File.expand_path('~/AppData/Local/nvim/init.vim')
+    File.open(myhome, 'a:utf-8', perm = 0o777) do |f|
+      f.puts <<-DOC
+if filereadable(expand('~/ddc_vim/init.vim'))
+  source ~/ddc_vim/init.vim
+endif
+      DOC
+    end
+  end
+end
+
+White.magic
