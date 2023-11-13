961d0ccbbeffff1f399920bc9bcb5fa6f4c5c631
Takayuki Kamiyama <karuma.reason@gmail.com>
2023-11-13 13:49:03 +0900

    Update.

--- lua/path_settings.lua
+++ lua/path_settings.lua
@@ -9,5 +9,6 @@ vim.g['ruby_host_prog'] = 'C:/current_ruby/3.2.2/bin/neovim-ruby-host'
 vim.g['python3_host_skip_check'] = 1
 
 -- denops settings
-vim.g['denops#deno'] = $HOME . '/scoop/shims/deno'
+home = os.getenv("HOME")
+vim.g['denops#deno'] = home .. '/scoop/shims/deno'
 vim.g['denops_disable_version_check'] = 1
