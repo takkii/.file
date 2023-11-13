d2a1c064bb3348059535924d5bf87f01ab903d08
Takayuki Kamiyama <karuma.reason@gmail.com>
2023-11-13 14:17:40 +0900

    Update.

--- lua/appearance.lua
+++ lua/appearance.lua
@@ -4,28 +4,27 @@ vim.cmd [[
 try
   colorscheme iceberg
   syntax enable
+  set guifont=Source\ Code\ Pro\ Semibold\:h9:sb
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme iceberg
   set background=dark
   syntax enable
+  set guifont=Source\ Code\ Pro\ Semibold\:h9:sb
 endtry
 ]]
 
 local options = {
-fileencodings = "utf-8,cp932",
-splitbelow = true,
-nobackup = true,
-noundofile = true,
-noswapfile = true,
-number = true,
-wildmenu = true,
-wildmode = "list:full",
-laststatus = 2
-guifont = "Source\ Code\ Pro\ Semibold\:h9:sb"
-backspace = "indent,eol,start"
-lines = 45
-columns = 124
-encoding = "utf-8"
+  fileencodings = "utf-8,cp932",
+  splitbelow = true,
+  nobackup = true,
+  noundofile = true,
+  noswapfile = true,
+  number = true,
+  wildmenu = true,
+  wildmode = "list:full",
+  laststatus = 2,
+  backspace = "indent,eol,start",
+  lines = 45,
+  columns = 124,
+  encoding = "utf-8",
 }
-
-cd ~/
