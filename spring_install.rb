f6237168348d552c7ea5ea4f31b45ef7acf1c91e
Takayuki Kamiyama <karuma.reason@gmail.com>
2023-11-14 12:47:20 +0900

    Update.

--- spring_install.rb
+++ spring_install.rb
@@ -11,7 +11,7 @@ class InstallerRunner
     Encoding.default_internal = 'UTF-8'
     Encoding.default_external = 'UTF-8'
   end
-  
+
   def self.run
     encoding_style
     if Dir.exist?(File.expand_path('~/ddc_spring_load'))
@@ -20,12 +20,22 @@ class InstallerRunner
       stdout_gt, _stderr_gt, _status_gt = Open3.capture3("git clone git@github.com:takkii/ddc_spring_load.git")
       stdout_gt
       FileUtils.mv("#{File.dirname(__FILE__)}/ddc_spring_load", File.expand_path('~/'))
+      puts 'Since there is already a folder, I deleted it and placed the specified folder.'
     else
       stdout_gt, _stderr_gt, _status_gt = Open3.capture3("git clone git@github.com:takkii/ddc_spring_load.git")
       stdout_gt
       FileUtils.mv("#{File.dirname(__FILE__)}/ddc_spring_load", File.expand_path('~/'))
+      puts 'The specified folder has been placed.'
     end
   end
 end
 
-InstallerRunner.run
+begin
+  InstallerRunner.run
+rescue StandardError => e
+  puts e.backtrace
+ensure
+  GC.compact
+end
+
+__END__
