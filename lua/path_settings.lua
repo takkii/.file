e6afd5a8c9e65779b836424ddd58147804ef704b
Takayuki Kamiyama <karuma.reason@gmail.com>
2023-11-19 15:01:34 +0900

    Update.

--- lua/path_settings.lua
+++ lua/path_settings.lua
@@ -1,8 +1,14 @@
+-- path_settings.
+
+if vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
+
+-- Windows settings.
+
 -- Python3
 vim.g['python3_host_prog'] = 'C:/Python3/python.exe'
 vim['dynamic_python_dll'] = 'C:/Python3/python.dll'
 
--- Ruby3
+-- Ruby3, custom path.
 vim.g['ruby_host_prog'] = 'C:/current_ruby/3.2.2/bin/neovim-ruby-host'
 
 -- Skip the check of neovim module
@@ -12,3 +18,66 @@ vim.g['python3_host_skip_check'] = 1
 home = os.getenv("HOME")
 vim.g['denops#deno'] = home .. '/scoop/shims/deno'
 vim.g['denops_disable_version_check'] = 1
+
+elseif vim.fn.has('osxdarwin') == 1 or vim.fn.has('osx') == 1 then
+
+-- MacOS rbenv settings.
+
+-- Home
+home = os.getenv("HOME")
+
+-- Python3
+vim.g['python3_host_prog'] = home .. '/.pyenv/shims/python'
+
+-- Ruby3
+vim.g['ruby_host_prog'] = home .. '/.rbenv/shims/neovim-ruby-host'
+
+-- Skip the check of neovim module
+vim.g['python3_host_skip_check'] = 1
+
+-- denops settings
+vim.g['denops#deno'] = home .. '/.deno/bin/deno'
+vim.g['denops_disable_version_check'] = 1
+
+
+elseif vim.fn.has('linux') == 1 then
+
+-- linux kernel anyenv settings.
+
+-- Home
+home = os.getenv("HOME")
+
+-- Python3
+vim.g['python3_host_prog'] = home .. '/.anyenv/envs/pyenv/shims/python'
+
+-- Ruby3
+vim.g['ruby_host_prog'] = home .. '/.anyenv/envs/rbenv/shims/neovim-ruby-host'
+
+-- Skip the check of neovim module
+vim.g['python3_host_skip_check'] = 1
+
+-- denops settings
+vim.g['denops#deno'] = home .. '/.deno/bin/deno'
+vim.g['denops_disable_version_check'] = 1
+
+else
+
+-- else anyenv settings.
+
+-- Home
+home = os.getenv("HOME")
+
+-- Python3
+vim.g['python3_host_prog'] = home .. '/.anyenv/envs/pyenv/shims/python'
+
+-- Ruby3
+vim.g['ruby_host_prog'] = home .. '/.anyenv/envs/rbenv/shims/neovim-ruby-host'
+
+-- Skip the check of neovim module
+vim.g['python3_host_skip_check'] = 1
+
+-- denops settings
+vim.g['denops#deno'] = home .. '/.deno/bin/deno'
+vim.g['denops_disable_version_check'] = 1
+
+end
