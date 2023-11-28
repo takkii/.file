679361b758162eee3ae482c4cbf7a6795fb52203
Takayuki Kamiyama <karuma.reason@gmail.com>
2023-11-28 14:24:19 +0900

    Update.

--- lua/path_settings.lua
+++ lua/path_settings.lua
@@ -4,27 +4,37 @@ if vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
 
 -- Windows settings.
 
--- Python3
-vim.g['python3_host_prog'] = 'C:/Python3/python.exe'
-vim['dynamic_python_dll'] = 'C:/Python3/python.dll'
+-- Home
+home = os.getenv("HOME")
 
 -- Use Scoop, Settings.
-home = os.getenv("HOME")
+
+-- Python3
+if vim.fn.isdirectory(home .. '/scoop/apps/python311/current') == 1 then
+  vim.g['python3_host_prog'] = home .. '/scoop/apps/python311/current/python.exe'
+  vim['dynamic_python_dll'] = home .. '/scoop/apps/python311/current/python3.dll'
+else
+  -- Python3 Installer PATH
+end
+
+-- Ruby3
 if vim.fn.isdirectory(home .. '/scoop/apps/ruby/current') == 1 then
   vim.g['ruby_host_prog'] = home .. '/scoop/apps/ruby/current/gems/bin/neovim-ruby-host'
--- mkdir Ruby3 Folder, into C drive
--- elseif vim.fn.isdirectory('C:/Ruby3') == 1 then
---  vim.g['ruby_host_prog'] = 'C:/Ruby3/bin/neovim-ruby-host'
 else
   -- RubyInstaller2 Install PATH
+end
+
+-- Deno
+if vim.fn.isdirectory(home .. '/scoop/apps/deno/current') == 1 then
+  vim.g['denops#deno'] = home .. '/scoop/shims/deno'
+else
+  -- Deno Install PATH
 end
 
 -- Skip the check of neovim module
 vim.g['python3_host_skip_check'] = 1
 
 -- denops settings
-home = os.getenv("HOME")
-vim.g['denops#deno'] = home .. '/scoop/shims/deno'
 vim.g['denops_disable_version_check'] = 1
 
 elseif vim.fn.has('osxdarwin') == 1 or vim.fn.has('osx') == 1 then
