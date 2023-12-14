67fdd1d4b3ec640c8e7a2af2ca170a1cf37934ea
Takayuki Kamiyama <karuma.reason@gmail.com>
2023-12-14 15:16:33 +0900

    Update.

--- lua/lua_folder_move.rb
+++ lua/lua_folder_move.rb
@@ -10,11 +10,13 @@ class White
     Encoding.default_external = 'UTF-8'
   end
 
+  # multi platform lua settings.
   def self.magic
     encoding_style
 
     host_os = RbConfig::CONFIG['host_os']
     case host_os
+
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       if Dir.exist?(File.expand_path('~/scoop/apps/neovim/current/bin/lua'))
         puts 'There is already a folder, ruby does nothing.'
@@ -23,13 +25,15 @@ class White
         FileUtils.cp(['./init.lua', './appearance.lua', './himekuri.lua', './settings.lua', './path_settings.lua', './runtimepath.lua', './spring_load.lua', './vim-com.lua', './vim-plug.lua', './ware_settings.lua'], File.expand_path('~/scoop/apps/neovim/current/bin/lua'))
         puts 'The specified file has been extracted.'
       end
+
     when /darwin|mac os/
-    if File.exist?(File.expand_path('~/neovim/.deps/usr/share/luajit-2.1/init.lua'))
-    puts 'There is already a folder, ruby does nothing.'
-    else
-    FileUtils.cp(['./init.lua', './appearance.lua', './himekuri.lua', './settings.lua', './path_settings.lua', './runtimepath.lua', './spring_load.lua', './vim-com.lua', './vim-plug.lua', './ware_settings.lua'], File.expand_path('~/neovim/.deps/usr/share/luajit-2.1'))
-    puts 'The specified file has been extracted.'
-    end
+      if File.exist?(File.expand_path('~/neovim/.deps/usr/share/luajit-2.1/init.lua'))
+        puts 'There is already a folder, ruby does nothing.'
+      else
+        FileUtils.cp(['./init.lua', './appearance.lua', './himekuri.lua', './settings.lua', './path_settings.lua', './runtimepath.lua', './spring_load.lua', './vim-com.lua', './vim-plug.lua', './ware_settings.lua'], File.expand_path('~/neovim/.deps/usr/share/luajit-2.1'))
+        puts 'The specified file has been extracted.'
+      end
+
     when /linux/
       if File.exist?(File.expand_path('~/neovim/.deps/usr/share/luajit-2.1/init.lua'))
         puts 'There is already a folder, ruby does nothing.'
@@ -37,6 +41,7 @@ class White
         FileUtils.cp(['./init.lua', './appearance.lua', './himekuri.lua', './settings.lua', './path_settings.lua', './runtimepath.lua', './spring_load.lua', './vim-com.lua', './vim-plug.lua', './ware_settings.lua'], File.expand_path('~/neovim/.deps/usr/share/luajit-2.1'))
         puts 'The specified file has been extracted.'
       end
+
     end
   end
 end
