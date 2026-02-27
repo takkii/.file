8fc3e5761cc0ca9e438bc472759787854a4a3d0a
Takayuki Kamiyama <karuma.reason@gmail.com>
2026-02-27 10:17:15 +0900

    Update.

--- lua/appearance.lua
+++ lua/appearance.lua
@@ -17,7 +17,6 @@ vim.cmd [[
   set wildmenu
   set wildmode=list:full
   set laststatus=2
-  set guifont=Source\ Code\ Pro\ Semibold\:h11:sb
   set backspace=indent,eol,start
   set encoding=utf-8
 
@@ -87,7 +86,6 @@ else
   set wildmenu
   set wildmode=list:full
   set laststatus=2
-  set guifont=Source\ Code\ Pro\ Semibold\:h11
   set backspace=indent,eol,start
   set encoding=utf-8
 
