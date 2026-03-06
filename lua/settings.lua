0c6125c1571c13c68b78d2f086c91c0c1eb7258d
Ｔ・Ｋ <karuma.reason@gmail.com>
2026-03-07 08:34:48 +0900

    Revert.

--- lua/settings.lua
+++ lua/settings.lua
@@ -3,6 +3,30 @@
 require('neoruby-debugger').setup()
 require('psycho').setup()
 
+-- Elixir Settings.
+local dap = require("dap")
+
+dap.adapters.mix_task = {
+  type = 'executable',
+  command = vim.fn.stdpath("data") .. '/mason/bin/elixir-ls-debugger';
+  args = {}
+}
+dap.configurations.elixir = {
+  {
+    type = "mix_task",
+    name = "mix test",
+    task = 'test',
+    taskArgs = {"--trace"},
+    request = "launch",
+    startApps = true, -- for Phoenix projects
+    projectDir = "${workspaceFolder}",
+    requireFiles = {
+      "test/**/test_helper.exs",
+      "test/**/*_test.exs"
+    }
+  },
+}
+
 -- dap-python, Python PATH.
 home = os.getenv("HOME")
 
